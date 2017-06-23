8c672c13e1d022813b29c7b9cc9b8eb4e16c522e
Milind Chabbi <mc29@smaug-2.u.hpl.hp.com>
2017-06-22 18:18:57 -0700

    Almost workinig version

--- builder.sh
+++ builder.sh
@@ -1,18 +1,21 @@
 set -ex
 
 LOC=`pwd`
+if false
+then
 cd  sst-core-7.1.0  
 ./autogen.sh
 cd $LOC/sst-macro-7.1.0
 ./bootstrap.sh
 cd $LOC
+fi
 
 #INSTALL_LOC="path/to/you/install/loc"
 INSTALL_LOC="/dev/shm/sst-install"
 mkdir -p $INSTALL_LOC
 
 
-if true
+if false
 then
 wget  https://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.gz/download -O boost_1_59_0.tar.gz
 tar zxvf boost_1_59_0.tar.gz
@@ -20,9 +23,9 @@ fi
 
 cd $LOC/sst-core-7.1.0
 export SST_CORE_HOME=$INSTALL_LOC/sstcore-7.1.0
-./configure --prefix=$SST_CORE_HOME
-make all -j `nproc`
-make install
+#./configure --prefix=$SST_CORE_HOME
+#make all -j `nproc`
+#make install
 export PATH=$SST_CORE_HOME/bin:$PATH
 
 
@@ -32,7 +35,7 @@ cd $LOC/boost_1_59_0
 ./bootstrap.sh --prefix=$INSTALL_LOC/boost_1_59_0-install/ --with-libraries="program_options,serialization,filesystem" --with-toolset=gcc
 ./b2 -j 12 --layout=tagged
 
-cd $LOC/sstmacro-7.1.0
+cd $LOC/sst-macro-7.1.0
 rm -rf build
 mkdir  build
 cd build
@@ -51,3 +54,4 @@ export PYTHONPATH=$INSTALL_LOC/include/python:$PYTHONPATH
 
 sst++ --help
 export SSTMAC_VERBOSE=1
+fi
