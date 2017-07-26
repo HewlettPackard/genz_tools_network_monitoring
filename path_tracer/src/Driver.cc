3b4c1c23452972f0f425e8b5ae3221ae282154c9
Adarsh Yoga <adarsh.yoga@hpe.com>
2017-07-25 17:55:17 -0700

    reverting monitor logger and adding call to construct path in Driver

--- path_tracer/src/Driver.cc
+++ path_tracer/src/Driver.cc
@@ -1,8 +1,10 @@
 #include "Log_Reader.h"
+#include "Path_Constructor.h"
 
 int main(int argc, char** argv) {
   Log_Reader reader(128,128,64);
 
-  
+  Path_Constructor pc(&reader);
+  pc.construct_all_paths();
   //reader.print_all_logs();
 }
--- sst-macro-7.1.0/sstmac/hardware/common/monitor_logger.h
+++ sst-macro-7.1.0/sstmac/hardware/common/monitor_logger.h
@@ -1,57 +1,36 @@
 #ifndef MONITOR_LOGGER_H_INCLUDED
 #define MONITOR_LOGGER_H_INCLUDED
 
-#include <sstmac/hardware/common/log_info.h>
-#include <sstmac/hardware/common/config_info.h>
-#include <sstmac/hardware/common/node_info.h>
 #include <fstream>
 #include <iostream>
+#include <stdio.h>
 
 namespace sstmac {
   namespace hw {
 
-    template<class T>
-    class monitor_logger
-    {
-    public:
-      monitor_logger(std::string filename) {
-	logger.open(filename);
-      }
-      
-      ~monitor_logger() {
-	logger.close();
-      }
-      
-      void recv(T* log) {
-	if (std::is_same<T, log_info>::value) {
-	  logger << ((log_info*)log)->from_addr << ","
-		 << ((log_info*)log)->to_addr << ","
-		 << ((log_info*)log)->message_id << ","
-		 << ((log_info*)log)->packet_id << ","
-		 << ((log_info*)log)->out_port << ","
-		 << ((log_info*)log)->next_hop_port << ","
-		 << ((log_info*)log)->arr_time << ","
-		 << (((log_info*)log)->head_leaves != 0 ? std::to_string(((log_info*)log)->head_leaves) : " ") << ","
-		 << (((log_info*)log)->tail_leaves != 0 ? std::to_string(((log_info*)log)->tail_leaves) : " ")
-		 << std::endl;
-	} else if (std::is_same<T, config_info>::value) {
-	  logger << ((config_info*)log)->from_id << ","
-		 << ((config_info*)log)->from_port << ","
-		 << ((config_info*)log)->to_id << ","
-		 << ((config_info*)log)->to_port << ","
-		 << std::endl;
-	} else if (std::is_same<T, node_info>::value) {
-	  logger << ((node_info*)log)->from_addr << ","
-		 << ((node_info*)log)->to_addr << ","
-		 << ((node_info*)log)->message_id << ","
-		 << ((node_info*)log)->send_time << ","
-		 << std::endl;	  
-	}
-      }
-      
-    protected:
-      std::ofstream logger;
-    };
+        template<class T>
+	      class monitor_logger
+	  {
+	  public:
+	    monitor_logger(std::string file) {
+	      logger = fopen(file.c_str(), "wb");
+	      //logger.open(file, std::ios::out | std::ios::binary);
+	    }
+
+	    ~monitor_logger() {
+	      fclose(logger);
+	      //logger.close();
+	    }
+
+	    void recv(T* log) {
+	      fwrite(log,sizeof(T), 1, logger);
+	      //logger.write(reinterpret_cast<char*>(log), sizeof(T));
+	    }
+
+	  protected:
+	    FILE* logger;
+	    //std::ofstream logger;
+	  };
 
   }
 }
