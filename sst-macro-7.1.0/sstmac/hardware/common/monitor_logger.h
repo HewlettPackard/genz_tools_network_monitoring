9d7cea0aef8bec569ed589ddd95ddfb1c23235a0
Adarsh Yoga <adarsh.yoga@hpe.com>
2017-07-25 17:56:10 -0700

    indenting monitor logger

--- sst-macro-7.1.0/sstmac/hardware/common/monitor_logger.h
+++ sst-macro-7.1.0/sstmac/hardware/common/monitor_logger.h
@@ -8,29 +8,29 @@
 namespace sstmac {
   namespace hw {
 
-        template<class T>
-	      class monitor_logger
-	  {
-	  public:
-	    monitor_logger(std::string file) {
-	      logger = fopen(file.c_str(), "wb");
-	      //logger.open(file, std::ios::out | std::ios::binary);
-	    }
+    template<class T>
+      class monitor_logger
+      {
+      public:
+	monitor_logger(std::string file) {
+	  logger = fopen(file.c_str(), "wb");
+	  //logger.open(file, std::ios::out | std::ios::binary);
+	}
 
-	    ~monitor_logger() {
-	      fclose(logger);
-	      //logger.close();
-	    }
+	~monitor_logger() {
+	  fclose(logger);
+	  //logger.close();
+	}
 
-	    void recv(T* log) {
-	      fwrite(log,sizeof(T), 1, logger);
-	      //logger.write(reinterpret_cast<char*>(log), sizeof(T));
-	    }
+	void recv(T* log) {
+	  fwrite(log,sizeof(T), 1, logger);
+	  //logger.write(reinterpret_cast<char*>(log), sizeof(T));
+	}
 
-	  protected:
-	    FILE* logger;
-	    //std::ofstream logger;
-	  };
+      protected:
+	FILE* logger;
+	//std::ofstream logger;
+      };
 
   }
 }
