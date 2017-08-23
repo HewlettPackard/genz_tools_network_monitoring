18f1a7c94002b61ea1e14c3ecb229d2a77ab15a3
Adarsh Yoga <adarsh.yoga@hpe.com>
2017-08-23 15:06:00 -0700

    added workshop paper template and fixed bug in path construction

--- path_tracer/src/Path_Constructor.cc
+++ path_tracer/src/Path_Constructor.cc
@@ -20,7 +20,7 @@ Path_Constructor::construct_path(node_info& n_info) {
   //First get the source node id
   uint32_t src_id = n_info.from_addr;
 
-  if (n_info.from_addr != 42 && n_info.to_addr != 42) return;
+  //if (n_info.from_addr != 42 && n_info.to_addr != 42) return;
 
   //Add node info to path vector
   path_info p_info;
