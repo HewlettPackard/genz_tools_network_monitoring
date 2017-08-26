548519b4a79e6225e19e67839f85c6f2e7f086ea
Adarsh Yoga <adarsh.yoga@hpe.com>
2017-08-25 17:05:06 -0700

    updated test programs

--- sst-macro-7.1.0/skeletons/large_small/main.cc
+++ sst-macro-7.1.0/skeletons/large_small/main.cc
@@ -4,10 +4,10 @@
 
 #define sstmac_app_name sendrecv
 
-#define ROW 1000
-#define COL (1L << 20)
+#define ROW 10
+#define COL (1L << 28)
 
-#define COL_SMALL (1L << 2)
+#define COL_SMALL (1L << 6)
 
 int main(int argc, char** argv)
 {
@@ -16,29 +16,29 @@ int main(int argc, char** argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  int** buf = new int*[ROW];
-  for (unsigned int i = 0; i < ROW; i++) {
-    buf[i] = new int[COL];
-  }
+  int* buf = new int[COL];
+  // for (unsigned int i = 0; i < ROW; i++) {
+  //   buf[i] = new int[COL];
+  // }
 
-  int** buf_small = new int*[ROW];
-  for (unsigned int i = 0; i < ROW; i++) {
-    buf_small[i] = new int[COL_SMALL];
-  }
+  int* buf_small = new int[COL_SMALL];
+  // for (unsigned int i = 0; i < ROW; i++) {
+  //   buf_small[i] = new int[COL_SMALL];
+  // }
   
   int tag = 42;
   if (rank == 0) {
     int partner = 7;
-    for (unsigned int i = 0; i < ROW; i++) {
-      MPI_Send(buf[i], COL, MPI_INT, partner, tag, MPI_COMM_WORLD);
-      MPI_Send(buf_small[i], COL_SMALL, MPI_INT, partner, tag, MPI_COMM_WORLD);
-    }
-  } else {
+    //for (unsigned int i = 0; i < ROW; i++) {
+    MPI_Send(buf, COL, MPI_INT, partner, tag, MPI_COMM_WORLD);
+    MPI_Send(buf_small, COL_SMALL, MPI_INT, partner, tag, MPI_COMM_WORLD);
+    //}
+  } else if (rank == 7){
     int partner = 0;
-    for (unsigned int i = 0; i < ROW; i++) {
-      MPI_Recv(buf[i], COL, MPI_INT, partner, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      MPI_Recv(buf_small[i], COL_SMALL, MPI_INT, partner, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
+    //for (unsigned int i = 0; i < ROW; i++) {
+    MPI_Recv(buf, COL, MPI_INT, partner, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Recv(buf_small, COL_SMALL, MPI_INT, partner, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    //}
   }
 
   MPI_Barrier(MPI_COMM_WORLD);
